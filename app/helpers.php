28a0fc33b27bb36330fbee642a762c210441e1f1
unknown <kapangajireh@gmail.com>
2019-12-30 21:31:28 +0100

    Queue mail for performance boost

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,7 @@
 <?php 
 
 function page_title($title){
-	$base_title = config('app.name')." - list of Artisans";
+	$base_title = config('app.name')." - List of Artisans";
 if ($title =="") {
 	return $base_title;
 } else {
